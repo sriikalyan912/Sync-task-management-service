efb6551b8d20db109fbc15d6bad33665d032f46f
Srii Kalyan <sriikalyan18@gmail.com>
2025-10-19 11:11:12 +0530

    Spring Boot setup completed

---
+++ src/main/java/com/sync/task_management_service/TaskManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sync.task_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementServiceApplication.class, args);
+	}
+
+}
